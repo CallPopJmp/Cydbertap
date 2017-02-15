34fab08a17f1f43d290df4a3dbfa328b2dc591d8
d3r3zz3d <ross.wheeler@googlemail.com>
2017-02-15 22:29:18 +0000

    Update process_pcap.sh

--- process_pcap.sh
+++ process_pcap.sh
@@ -5,8 +5,8 @@ currentDate="$(/bin/date +'%Y%m%d')"
 #currentDate="20170206"
 
 ##Environment variables:
-pcapPath="/pcaps/"
-objPath="/objects/"
+pcapPath="/home/pi/cydbertap/pcaps/"
+objPath="/home/pi/cydbertap/objects/"
 capTipperPath="FULL_PATH_TO_CAPTIPPER_HERE.py"
 #echo "debug: currentD: " $currentDate
 #echo "debug: pcapPath: " $pcapPath
@@ -24,9 +24,9 @@ echo "debug: CapTipper processing file: " $fileToProcess
 sudo /usr/bin/python $capTipperPath $fileToProcess -d $objPath/$currentDate/ 2>$objPath/$currentDate/capTipper_errors.log 1>$objPath/$currentDate/capTipper_stdout.log
 
 ##Extract IOCs from output:
-/usr/sbin/tcpdump -Annr $fileToProcess | egrep "\b((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.|$)){4}\b" -o | cut -d\. -f1,2,3,4 | sort | uniq -c | sort -nr > $objPath/$currentDate/$currentDate.IP.histogram.txt
+/usr/sbin/tcpdump -Annr $fileToProcess | egrep "\b((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.|$)){4}\b" -o | cut -d\. -f1,2,3,4 | sort | uniq -c | sort -nr > $objPath/$currentDate/$currentDate.IP.counts.txt
 /usr/sbin/tcpdump -Annr $fileToProcess | grep "User-Agent" | sort | uniq -c | sort -nr > $objPath/$currentDate/$currentDate.UA.histogram.txt
-/usr/sbin/tcpdump -Annr $fileToProcess | egrep "\:\/\/([^\/\"]{2,100})" -o | cut -d\/ -f3 | sort | uniq -c | sort -nr > $objPath/$currentDate/$currentDate.Domains.histogram.txt
+/usr/sbin/tcpdump -Annr $fileToProcess | egrep "\:\/\/([^\/\"]{2,100})" -o | cut -d\/ -f3 | sort | uniq -c | sort -nr > $objPath/$currentDate/$currentDate.Domains.counts.txt
 
 #Finished message
 echo "Job Finished. All output was written to: " $dateDir
